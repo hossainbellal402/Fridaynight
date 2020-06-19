128eaa1024531922f3a5520458a72eba09e36d73
hossainbellal402 <66582075+hossainbellal402@users.noreply.github.com>
2020-06-19 02:01:58 -0400

    zxzx

--- src/fridaytry/Fridayhomepratice.java
+++ src/fridaytry/Fridayhomepratice.java
@@ -1,10 +1,23 @@
 package fridaytry;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class Fridayhomepratice {
 
-	public static void main(String[] args) {
+	@Test
+	public void openBrowser() {
 		
 
+		System.setProperty( "webdriver.chrome.driver", "C:\\Users\\bella\\eclipse-workspace\\Fridaynight\\Drivers\\chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+	  driver.get( "https:www.amazon.com/");
+		
+		
+		
+		
+		
 	}
 
 }
